fb118f90301713f7235229a86717c59e7553aece
VK <belsynth@gmail.com>
2018-12-17 13:40:50 +0300

    Добавил в utils.js функцию debounce

--- js/utils.js
+++ js/utils.js
@@ -26,6 +26,18 @@ window.utils = (function () {
         return two;
       }
       return five;
+    },
+    debounce: function (cb, interval) {
+      var lastTimeout = null;
+      return function () {
+        var parameters = arguments;
+        if (lastTimeout) {
+          window.clearTimeout(lastTimeout);
+        }
+        lastTimeout = window.setTimeout(function () {
+          cb.apply(null, parameters);
+        }, interval);
+      };
     }
   };
 
